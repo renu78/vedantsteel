6c88e71c1fe858ac855f0b13fef78e9765634c2e
renu78 <renukakul298@gmail.com>
2025-05-09 14:07:54 +0530

    update prisma

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -143,7 +143,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
@@ -153,11 +153,12 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": "postgresql://postgres:Renuka@localhost:5432/mydb?schema=public"
+        "value": null
       }
     }
   },
