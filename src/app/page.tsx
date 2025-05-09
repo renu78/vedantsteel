3148b2de5904360cbe46f4ca213c0fcdb34e4d86
renu78 <renukakul298@gmail.com>
2025-05-09 12:46:50 +0530

    redirect to home

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+// File: app/home/page.tsx
+import { redirect } from 'next/navigation';
+
+export default function HomeRedirect() {
+  // Redirect to homepage or another route
+  redirect('/home');
+}
